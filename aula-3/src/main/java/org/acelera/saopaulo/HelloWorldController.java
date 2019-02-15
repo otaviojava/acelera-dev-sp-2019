07bb0f15dfda484742729ab3a965d1c5ebf51fce
Otavio Santana <otaviopolianasantana@gmail.com>
2019-02-15 11:37:00 -0200

    creates the hello world

---
+++ aula-3/src/main/java/org/acelera/saopaulo/HelloWorldController.java
@@ -0,0 +1,22 @@
+package org.acelera.saopaulo;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/hello-world")
+    @ResponseBody
+    public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+}
