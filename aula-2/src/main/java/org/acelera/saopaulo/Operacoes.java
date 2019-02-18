045b78ec4e5c5424e01cae00fb55d4be932c398d
André Barranco <b.andrefilipe@gmail.com>
2019-02-18 15:09:12 -0300

    Remove ruído em if/throws/return

--- aula-2/src/main/java/org/acelera/saopaulo/Operacoes.java
+++ aula-2/src/main/java/org/acelera/saopaulo/Operacoes.java
@@ -1,35 +1,41 @@
 package org.acelera.saopaulo;
 
-public class Operacoes {
+public class Operacoes
+{
 
-	public int soma() {
-		Operacao<Integer, Integer> op = (a, b) -> {
+	public int soma()
+	{
+		Operacao<Integer, Integer> op = (a, b) ->
+		{
 			return a + b;
 		};
 		return op.apply(3, 4);
 	}
 
-	public int subtracao() {
-		Operacao<Integer, Integer> op = (a, b) -> {
+	public int subtracao()
+	{
+		Operacao<Integer, Integer> op = (a, b) ->
+		{
 			return a - b;
 		};
 		return op.apply(3, 4);
 	}
 
-	public int multiplicacao() {
-		Operacao<Integer, Integer> op = (a, b) -> {
+	public int multiplicacao()
+	{
+		Operacao<Integer, Integer> op = (a, b) ->
+		{
 			return a * b;
 		};
 		return op.apply(3, 4);
 	}
 
-	public int divisao() {
-		Operacao<Integer, Integer> op = (a, b) -> {
-			if (b != 0) {
-				return a + b;
-			} else {
-				throw new ArithmeticException("Não é possível realizar divisões por zero !");
-			}
+	public int divisao()
+	{
+		Operacao<Integer, Integer> op = (a, b) ->
+		{
+			if (b == 0) throw new ArithmeticException("Não é possível realizar divisões por zero !");
+			return a / b;
 		};
 		return op.apply(3, 4);
 	}
