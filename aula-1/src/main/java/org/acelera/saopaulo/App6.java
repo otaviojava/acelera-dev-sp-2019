09a3a77c2c646b94e4b9a0dfc2e554670f74d5ae
André Barranco <b.andrefilipe@gmail.com>
2019-02-17 21:09:30 -0300

    Remove fluxo redundante com if/else

--- aula-1/src/main/java/org/acelera/saopaulo/App6.java
+++ aula-1/src/main/java/org/acelera/saopaulo/App6.java
@@ -1,29 +1,32 @@
 package org.acelera.saopaulo;
 
-public class App6 {
-	public static void main(String[] args) {
-
-		Operacao<Integer, Integer> soma = (a, b) -> {
+public class App6
+{
+	public static void main(String[] args)
+	{
+		Operacao<Integer, Integer> soma = (a, b) ->
+		{
 			return a + b;
 		};
 		soma.operar(5, 6);
 
-		Operacao<Integer, Integer> subtracao = (a, b) -> {
+		Operacao<Integer, Integer> subtracao = (a, b) ->
+		{
 			return a - b;
 		};
 		subtracao.operar(5, 6);
 
-		Operacao<Integer, Integer> multiplicacao = (a, b) -> {return a * b;	};
+		Operacao<Integer, Integer> multiplicacao = (a, b) ->
+		{
+			return a * b;
+		};
 		multiplicacao.operar(5, 6);
 
-		Operacao<Integer, Integer> divisao = (a, b) -> {
-			if (b != 0) {
-				return a / b;
-			} else {
-				throw new ArithmeticException("Não é possível realizar divisões por zero !");
-			}
+		Operacao<Integer, Integer> divisao = (a, b) ->
+		{
+			if (b == 0) throw new ArithmeticException("Não é possível realizar divisões por zero !");
+			return a / b;
 		};
-
 		divisao.operar(5, 6);
 	}
 }
