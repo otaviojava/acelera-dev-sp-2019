cd9f1391d4c1e0c6de9da9729d1bacadbc92c8f6
alan-nishimori <alan.nishimori@gmail.com>
2019-02-26 22:13:46 -0300

    remoção de exs de aula

--- aula-3/src/main/java/org/acelera/saopaulo/estoque/ProdutoRepository.java
+++
@@ -1,8 +0,0 @@
-package org.acelera.saopaulo.estoque;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProdutoRepository extends CrudRepository<Produto, String> {
-}
