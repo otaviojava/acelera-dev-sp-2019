e7d79d2d28232317a5fb60294381c615c824ebb1
klolivei <kaueluiseng@gmail.com>
2019-02-28 15:55:18 -0300

    Delete import nao usado

--- aula-3/src/main/java/org/acelera/saopaulo/soccer/service/JogadorService.java
+++ aula-3/src/main/java/org/acelera/saopaulo/soccer/service/JogadorService.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
-import javax.xml.ws.WebServiceException;
 import java.util.ArrayList;
 import java.util.List;
 
